8a65eeff6c94c415fac035ba95c1a59db3fc4dde
taewan <taewan2002@naver.com>
2023-05-10 15:40:05 +0900

    fix: 하단 네비게이션 바 텍스트 제거

--- lib/src/widgets/bottom_navigation_bar.dart
+++ lib/src/widgets/bottom_navigation_bar.dart
@@ -13,11 +13,11 @@ class CustomBottomNavigationBar extends StatelessWidget {
       onTap: onTap,
       type: BottomNavigationBarType.fixed, // 아이템이 4개 이상일 경우 필요합니다.
       items: [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-        BottomNavigationBarItem(icon: Icon(Icons.auto_stories), label: 'Dream'),
-        BottomNavigationBarItem(icon: Icon(Icons.whatshot), label: 'Hot'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'MyPage'),
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.auto_stories), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.whatshot), label: ''),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
       ],
     );
   }
