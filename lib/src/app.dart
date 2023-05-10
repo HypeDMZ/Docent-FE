b3b8698905556ebc87dad9933ddf9dfb0d4fc958
taewan <taewan2002@naver.com>
2023-05-10 15:39:44 +0900

    feat: 다크모드 설정 추가

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:docent/src/login_page.dart';
 
 class App extends StatelessWidget {
+  final bool isDarkMode = false; // 다크 모드: true, 라이트 모드: false
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Docent',
       theme: ThemeData(
         fontFamily: 'NanumGothic',
-        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+        ),
       ),
+      darkTheme: ThemeData(
+        fontFamily: 'NanumGothic',
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+        ),
+      ),
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light, // 여기에서 모드를 변경합니다.
+      builder: (BuildContext context, Widget? child) {
+        return AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Theme.of(context).brightness == Brightness.light
+                ? Brightness.dark
+                : Brightness.light,
+          ),
+          child: Material(child: child),
+        );
+      },
       home: LoginPage(),
     );
   }
