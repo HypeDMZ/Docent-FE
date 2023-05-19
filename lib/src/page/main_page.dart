44f754d8cb6cf5709c6002c6fa34bcc0747a2629
taewan <taewan2002@naver.com>
2023-05-19 18:43:09 +0900

    fix: 메인 페이지 사용하지 않는 라이브러리 제거

--- lib/src/page/main_page.dart
+++ lib/src/page/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../feature/common.dart';
 import '../feature/apiService.dart';
 import '../feature/token.dart';
